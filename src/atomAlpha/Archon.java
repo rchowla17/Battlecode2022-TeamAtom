03f34bca4fcbe36198c0af39925354c7d6694dfa
Kevin Lee <123kevinlee@gmail.com>
2022-01-04 02:47:16 -0500

    initial spawning sequence

--- src/atomAlpha/Archon.java
+++ src/atomAlpha/Archon.java
@@ -3,30 +3,87 @@ package atomAlpha;
 import battlecode.common.*;
 import java.util.*;
 
+import org.mockito.internal.matchers.StartsWith;
+
 public class Archon {
     static Queue<RobotType> startSpawnOrder = new LinkedList<RobotType>();
+    static int startSpawn = 0;
 
     static void runArchon(RobotController rc) throws GameActionException {
-        while (!startSpawnOrder.isEmpty()) {
+        while (startSpawn <= 9) {
             gameStartSequence(rc);
         }
     }
 
     //initial starting logic
     public static void gameStartSequence(RobotController rc) throws GameActionException {
-        RobotType type = startSpawnOrder.peek();
-        Direction dir = openSpawnLocation(rc, type);
-        if (rc.canBuildRobot(type, dir)) {
-            rc.buildRobot(startSpawnOrder.poll(), dir);
+        switch (startSpawn) {
+            case 0:
+                if (rc.canBuildRobot(RobotType.MINER, Direction.NORTH)) {
+                    rc.buildRobot(RobotType.MINER, Direction.NORTH);
+                    startSpawn++;
+                }
+                break;
+            case 1:
+                if (rc.canBuildRobot(RobotType.MINER, Direction.EAST)) {
+                    rc.buildRobot(RobotType.MINER, Direction.EAST);
+                    startSpawn++;
+                }
+                break;
+            case 2:
+                if (rc.canBuildRobot(RobotType.MINER, Direction.SOUTH)) {
+                    rc.buildRobot(RobotType.MINER, Direction.SOUTH);
+                    startSpawn++;
+                }
+                break;
+            case 3:
+                if (rc.canBuildRobot(RobotType.MINER, Direction.WEST)) {
+                    rc.buildRobot(RobotType.MINER, Direction.WEST);
+                    startSpawn++;
+                }
+                break;
+            case 4:
+                if (rc.canBuildRobot(RobotType.BUILDER, Direction.NORTH)) {
+                    rc.buildRobot(RobotType.BUILDER, Direction.NORTH);
+                    startSpawn++;
+                }
+                break;
+            case 5:
+                if (rc.canBuildRobot(RobotType.MINER, Direction.WEST)) {
+                    rc.buildRobot(RobotType.MINER, Direction.WEST);
+                    startSpawn++;
+                }
+                break;
+            case 6:
+                if (rc.canBuildRobot(RobotType.BUILDER, Direction.SOUTH)) {
+                    rc.buildRobot(RobotType.BUILDER, Direction.SOUTH);
+                    startSpawn++;
+                }
+                break;
+            case 7:
+                if (rc.canBuildRobot(RobotType.MINER, Direction.EAST)) {
+                    rc.buildRobot(RobotType.MINER, Direction.EAST);
+                    startSpawn++;
+                }
+                break;
+            case 8:
+                if (rc.canBuildRobot(RobotType.BUILDER, Direction.WEST)) {
+                    rc.buildRobot(RobotType.BUILDER, Direction.WEST);
+                    startSpawn++;
+                }
+                break;
+            case 9:
+                if (rc.canBuildRobot(RobotType.BUILDER, Direction.EAST)) {
+                    rc.buildRobot(RobotType.BUILDER, Direction.EAST);
+                    startSpawn++;
+                }
+                break;
         }
     }
 
     //unit initilization 
     public static void init() {
-        startSpawnOrder.add(RobotType.MINER);
-        startSpawnOrder.add(RobotType.MINER);
-        startSpawnOrder.add(RobotType.MINER);
-        startSpawnOrder.add(RobotType.MINER);
+
     }
 
     //returns open spawn direction
