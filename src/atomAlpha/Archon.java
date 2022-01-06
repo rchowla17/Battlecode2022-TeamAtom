8eac48c4ea48b74f7efb59880e973c66a8b1b963
Kevin Lee <123kevinlee@gmail.com>
2022-01-05 20:22:55 -0500

    removed scouts

--- src/atomAlpha/Archon.java
+++ src/atomAlpha/Archon.java
@@ -82,8 +82,8 @@ public class Archon {
                 }
                 break;
             case 4:
-                if (rc.canBuildRobot(RobotType.BUILDER, Direction.NORTH)) {
-                    rc.buildRobot(RobotType.BUILDER, Direction.NORTH);
+                if (rc.canBuildRobot(RobotType.MINER, Direction.NORTH)) {
+                    rc.buildRobot(RobotType.MINER, Direction.NORTH);
                     startSpawn++;
                 }
                 break;
@@ -94,8 +94,8 @@ public class Archon {
                 }
                 break;
             case 6:
-                if (rc.canBuildRobot(RobotType.BUILDER, Direction.SOUTH)) {
-                    rc.buildRobot(RobotType.BUILDER, Direction.SOUTH);
+                if (rc.canBuildRobot(RobotType.MINER, Direction.SOUTH)) {
+                    rc.buildRobot(RobotType.MINER, Direction.SOUTH);
                     startSpawn++;
                 }
                 break;
@@ -106,14 +106,14 @@ public class Archon {
                 }
                 break;
             case 8:
-                if (rc.canBuildRobot(RobotType.BUILDER, Direction.WEST)) {
-                    rc.buildRobot(RobotType.BUILDER, Direction.WEST);
+                if (rc.canBuildRobot(RobotType.MINER, Direction.WEST)) {
+                    rc.buildRobot(RobotType.MINER, Direction.WEST);
                     startSpawn++;
                 }
                 break;
             case 9:
-                if (rc.canBuildRobot(RobotType.BUILDER, Direction.EAST)) {
-                    rc.buildRobot(RobotType.BUILDER, Direction.EAST);
+                if (rc.canBuildRobot(RobotType.MINER, Direction.EAST)) {
+                    rc.buildRobot(RobotType.MINER, Direction.EAST);
                     startSpawn++;
                 }
                 break;
